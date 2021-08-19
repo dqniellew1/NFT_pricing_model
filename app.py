7bb06e6e9585fe22f0ce894ceecbbd000d5eceb6
dqniellew1 <dqniellew@gmail.com>
2021-08-19 13:51:14 +0800

    added api

--- app.py
+++ app.py
@@ -2,10 +2,59 @@
 import numpy as np
 import pandas as pd
 from flask import Flask, render_template, request
+from flask_restful import reqparse, abort, Api, Resource
 import joblib
 
 app = Flask(__name__)
+api = Api(app)
+
+# load trained model
 model = joblib.load("models/rf_ppg_model.joblib")
+
+pengus = pd.read_csv('data/data.csv', index_col=0)
+
+
+features = [f for f in pengus.columns if f not in ('id','token_id','image_url', 'permalink','address', 
+                                            'last_sold_token','last_sold_eth','last_sold_usd','listing_eth_price',
+                                            'payment_token.usd_price', 'stats.floor_price',
+                                            'last_sold_usd_log','stats.num_owners',
+                                            'Background','Body', 'Face', 'Head', 'Skin','preds_usd', 'last_sold_date',
+                                                'mean_sale_eth_day','max_sale_eth_day','Trait_count_avg_price','last_sold_eth_log')]
+
+
+#argument parsing
+parser = reqparse.RequestParser()
+parser.add_argument('nft_id', type=int)
+
+class PredictPrice(Resource):
+    def get(self):
+        # use parser and find price for the ID
+        args = parser.parse_args(strict=True)
+        nft_id = args['nft_id']
+        nft_id = int(nft_id)
+
+        preds_usd = model.predict(pengus.loc[pengus['token_id'] == nft_id][features])
+
+        #preds_usd = np.stack([t.predict(pengus[features]) for t in model.estimators_])
+        pengus.loc[pengus['token_id'] == nft_id, 'preds_usd'] = np.round(np.expm1(preds_usd[-1]), 2)
+        pengus.loc[pengus['token_id'] == nft_id, 'preds_eth'] = pengus['preds_usd'] / pengus['payment_token.usd_price'].median()
+        pengus.loc[pengus['token_id'] == nft_id, 'preds_eth'] = pengus['preds_eth'] * 1.2
+
+        prediction = pengus.loc[pengus['token_id'] == nft_id, ['token_id','image_url','permalink','last_sold_usd','last_sold_eth','preds_usd','preds_eth']]
+        if prediction['last_sold_eth'].values <= pengus['stats.floor_price'].median():
+            prediction['preds_eth'] = pengus['stats.floor_price'].median() * 1.2
+
+        penguin_ID = prediction['token_id'].values[0]
+        penguin_img = prediction['image_url'].values[0]
+        penguin_prediction = np.round(prediction['preds_eth'].values[0], 2)
+        penguin_real = prediction['last_sold_eth'].values[0]
+        opensea = prediction['permalink'].values[0]
+                
+        # create JSON object
+        output = {'nft_id': int(penguin_ID), 'image_link': penguin_img, 'prediction':penguin_prediction, 'opensea-link':opensea}
+        return output
+
+api.add_resource(PredictPrice, '/api')
 
 #default page of our web-app
 @app.route('/')
@@ -22,15 +71,6 @@ def predict():
         prediction = request.form["penguinid"]
 
 #     #For rendering results on HTML GUI
-    pengus = pd.read_csv('data/data.csv', index_col=0)
-
-
-    features = [f for f in pengus.columns if f not in ('id','token_id','image_url', 'permalink','address', 
-                                                'last_sold_token','last_sold_eth','last_sold_usd','listing_eth_price',
-                                                'payment_token.usd_price', 'stats.floor_price',
-                                                'last_sold_usd_log','stats.num_owners',
-                                                'Background','Body', 'Face', 'Head', 'Skin','preds_usd', 'last_sold_date',
-                                                 'mean_sale_eth_day','max_sale_eth_day','Trait_count_avg_price','last_sold_eth_log')]
     id = int(request.form.get("penguinid"))
     preds_usd = model.predict(pengus.loc[pengus['token_id'] == id][features])
 
@@ -52,7 +92,7 @@ def predict():
     # .py -> HTML
     return render_template('index.html', ID = 'Penguin ID: {}'.format(penguin_ID), penguinimage = penguin_img, 
         opensea_link = opensea,
-        predictions = 'The predicted value of your penguin is: {} Ξ and it last sold for is: {} Ξ'.format(penguin_prediction, penguin_real))
+        predictions = 'The predicted value of your penguin is: {} Ξ.'.format(penguin_prediction))
 
     
 if __name__ == "__main__":
