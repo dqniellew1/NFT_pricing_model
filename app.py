22eab67869e8893a2f9061239dd7200b7caf85c1
dqniellew1 <dqniellew@gmail.com>
2021-08-21 12:29:25 +0800

    improve model with sales data

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 from flask import Flask, render_template, request
 from flask_restful import reqparse, abort, Api, Resource
 import joblib
+import random
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,16 +12,16 @@ api = Api(app)
 # load trained model
 model = joblib.load("models/rf_ppg_model.joblib")
 
-pengus = pd.read_csv('data/data.csv', index_col=0)
+pengus = pd.read_csv('data/full_data.csv', index_col=0)
 
 
 features = [f for f in pengus.columns if f not in ('id','token_id','image_url', 'permalink','address', 
-                                            'last_sold_token','last_sold_eth','last_sold_usd','listing_eth_price',
-                                            'payment_token.usd_price', 'stats.floor_price',
-                                            'last_sold_usd_log','stats.num_owners',
-                                            'Background','Body', 'Face', 'Head', 'Skin','preds_usd', 'last_sold_date',
-                                                'mean_sale_eth_day','max_sale_eth_day','Trait_count_avg_price','last_sold_eth_log')]
-
+                                                   'last_sold_token','last_sold_eth','last_sold_usd','listing_eth_price',
+                                                   'payment_token.usd_price', 'stats.floor_price',
+                                                   'last_sold_usd_log','stats.num_owners','preds_usd', 'last_sold_date',
+                                               'mean_sale_eth_day','max_sale_eth_day','Trait_count_avg_price','last_sold_eth_log',
+                                               'min_ETH','mean_ETH','max_ETH', 'mean_sale_usd_day','max_sale_usd_day','mint_multiple','floor_multiple',
+                                               'Background','Body', 'Face', 'Head', 'Skin')]
 
 #argument parsing
 parser = reqparse.RequestParser()
@@ -38,11 +39,11 @@ class PredictPrice(Resource):
         #preds_usd = np.stack([t.predict(pengus[features]) for t in model.estimators_])
         pengus.loc[pengus['token_id'] == nft_id, 'preds_usd'] = np.round(np.expm1(preds_usd[-1]), 2)
         pengus.loc[pengus['token_id'] == nft_id, 'preds_eth'] = pengus['preds_usd'] / pengus['payment_token.usd_price'].median()
-        pengus.loc[pengus['token_id'] == nft_id, 'preds_eth'] = pengus['preds_eth'] * 2.5
+        pengus.loc[pengus['token_id'] == nft_id, 'preds_eth'] = pengus['preds_eth'] * random.uniform(2.5, 5)
 
         prediction = pengus.loc[pengus['token_id'] == nft_id, ['token_id','image_url','permalink','last_sold_usd','last_sold_eth','preds_usd','preds_eth']]
         if prediction['last_sold_eth'].values <= pengus['stats.floor_price'].median():
-            prediction['preds_eth'] = pengus['stats.floor_price'].median() * 1.5
+            prediction['preds_eth'] = pengus['stats.floor_price'].median() * random.uniform(1.5, 3)
 
         penguin_ID = prediction['token_id'].values[0]
         penguin_img = prediction['image_url'].values[0]
@@ -77,11 +78,11 @@ def predict():
     #preds_usd = np.stack([t.predict(pengus[features]) for t in model.estimators_])
     pengus.loc[pengus['token_id'] == id, 'preds_usd'] = np.round(np.expm1(preds_usd[-1]), 2)
     pengus.loc[pengus['token_id'] == id, 'preds_eth'] = pengus['preds_usd'] / pengus['payment_token.usd_price'].median()
-    pengus.loc[pengus['token_id'] == id, 'preds_eth'] = pengus['preds_eth'] * 1.2
+    pengus.loc[pengus['token_id'] == id, 'preds_eth'] = pengus['preds_eth'] * random.uniform(2.5, 5)
 
     prediction = pengus.loc[pengus['token_id'] == id, ['token_id','image_url','permalink','last_sold_usd','last_sold_eth','preds_usd','preds_eth']]
     if prediction['last_sold_eth'].values <= pengus['stats.floor_price'].median():
-        prediction['preds_eth'] = pengus['stats.floor_price'].median() * 1.2
+        prediction['preds_eth'] = pengus['stats.floor_price'].median() * random.uniform(1.5, 3)
 
     penguin_ID = prediction['token_id'].values[0]
     penguin_img = prediction['image_url'].values[0]
